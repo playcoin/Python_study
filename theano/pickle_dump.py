cbfcb8c59a2579f103bd8ebb64bd64fd600a30f3
playcoin <seuplaycoin@gmail.com>
2012-11-21 16:37:31 +0800

    rename the file 'pickle.py' to 'pickle_dump.py'

--- theano/pickle_dump.py
+++ theano/pickle_dump.py
@@ -1,7 +1,7 @@
 import cPickle
 
 # open file
-f = file('dump/obj.save', 'wb')
+f = file('obj.save', 'wb')
 
 # initial a variable
 my_obj = [1, 2, 3]
@@ -13,4 +13,4 @@ f1 = file('dump/objs.save', 'wb')
 for obj in [1, 2, 3, 4] :
 	cPickle.dump(obj, f1, protocol=cPickle.HIGHEST_PROTOCOL)
 
-f.close()
+f1.close()
