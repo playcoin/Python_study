94a8b3f82395c4d0a5c91cc0d889e586893e0844
playcoin <seuplaycoin@gmail.com>
2012-11-27 18:37:29 +0800

    module and class

---
+++ general/mystuff.py
@@ -0,0 +1,18 @@
+# def apple():
+# 	print "I am an apple"
+
+
+# variable1 = "I am a variable"
+
+# new version
+class Mystuff(object):
+
+	def __init__(self):
+		self.tangerine = "And now a thousand years between"
+
+	def apple():
+		print "I AM CLASS APPLES"
+
+thing = Mystuff()
+
+print thing.tangerine
